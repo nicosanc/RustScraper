ae70f81ce803755068cfb265262d0901aeb05e97
nicosanc <nicosanchez0411@gmail.com>
2024-04-05 12:09:19 -0400

    -

---
+++ quotes_scraper/target/debug/deps/quotes_scraper-2724e0e1d095b6e1.d
@@ -0,0 +1,5 @@
+C:\Users\nicos\RustScraper\quotes_scraper\target\debug\deps\libquotes_scraper-2724e0e1d095b6e1.rmeta: src\main.rs
+
+C:\Users\nicos\RustScraper\quotes_scraper\target\debug\deps\quotes_scraper-2724e0e1d095b6e1.d: src\main.rs
+
+src\main.rs:
---
+++ quotes_scraper/target/debug/deps/quotes_scraper-446efdcc58a3a0c2.d
@@ -0,0 +1,5 @@
+C:\Users\nicos\RustScraper\quotes_scraper\target\debug\deps\libquotes_scraper-446efdcc58a3a0c2.rmeta: src\main.rs
+
+C:\Users\nicos\RustScraper\quotes_scraper\target\debug\deps\quotes_scraper-446efdcc58a3a0c2.d: src\main.rs
+
+src\main.rs:
